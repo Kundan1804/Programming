037075bb32065c971cb1d659ed7057d40ccb8e8a
Kundan <kundanagrawal18@gmail.com>
2024-10-13 18:42:21 +0530

    Question 1

---
+++ sprint-1/Q1.cpp
@@ -0,0 +1,8 @@
+// Determining Even/Odd Numbers
+// Difficulty: Easy
+// Topics: Basic Programming
+// Description: Write a program to check whether a number is even or odd.
+// Example:
+// Input: number = 4
+// Output: Even
+
